72963c47f8f8302595adc942eba7ef1fa94e5884
JaeHyeok29 <rlawogur263@naver.com>
2023-09-05 17:10:46 +0900

    주사위&도서관

---
+++ src/main/Library.java
@@ -0,0 +1,27 @@
+package main;
+
+import org.junit.Test;
+
+public class Library {
+
+    public Library(int i) {}
+
+    public void add(String string) {
+        
+    }
+
+    public int getTotalBookCount() {
+        if (maxBookCount <0)
+            throw new 
+        return 0;
+    }
+
+    public boolean find(String string) {
+        return false;
+    }
+
+    public void delete(String string) {
+        
+    }
+
+}
